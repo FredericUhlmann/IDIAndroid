4554ea8431f15566636ebf5a20e42c634352ab3e
Powuser <polalejandre@gmail.com>
2017-01-04 22:52:11 +0100

    draweractivity

---
+++ app/src/main/java/com/example/pr_idi/mydatabaseexample/Home.java
@@ -0,0 +1,12 @@
+package com.example.pr_idi.mydatabaseexample;
+
+import android.os.Bundle;
+
+public class Home extends DrawerActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
