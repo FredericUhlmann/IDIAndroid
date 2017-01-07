bb3e37351da01a5c0a6dbe223dd6bf0c31b74539
Frederic Uhlmann <f.w.uhlmann@gmail.com>
2017-01-08 00:04:54 +0100

    recycler

---
+++ app/src/main/java/com/example/pr_idi/mydatabaseexample/Filmoteca.java
@@ -0,0 +1,14 @@
+package com.example.pr_idi.mydatabaseexample;
+
+import android.os.Bundle;
+
+public class Filmoteca extends DrawerActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filmoteca);
+        setTitle("Filmoteca");
+    }
+
+}
